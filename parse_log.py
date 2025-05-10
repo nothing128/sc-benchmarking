e8953474abe7fa19e6847bc2e1f5f6dacb56bdb6
nothing128 <mingyau.lam@gmail.com>
2025-05-10 01:39:17 -0400

    update main

--- parse_log.py
+++ parse_log.py
@@ -1,13 +1,14 @@
 import os
 import numpy as np
+import sys
 
 work_dir = "projects/rrg-wainberg/lamming6/sc-benchmarking"
 data_dir = "single-cell/SEAAD/subsampled"
 
 
 def parse_log(log_file: str, out: str):
-    # path = os.path.join(work_dir, log_file)
-    path = log_file  # for local testing
+    path = os.path.join(work_dir, log_file)
+    # path = log_file  # for local testing
     with open(path, "r") as f:
         next(f)
         next(f)
@@ -24,7 +25,7 @@ def parse_log(log_file: str, out: str):
 
             while curr_line != "":  # read until EOF
                 if curr_line[0] == "L":
-                    params=curr_line.split("(")[1][:-2]
+                    params = curr_line.split("(")[1][:-2]
                     output.write(f"Params: {params}\n")
                     curr_line = f.readline()
                     mem = np.array([[-1.0, -1.0, -1.0]])  # RSS, VSZ, %MEM
@@ -35,7 +36,7 @@ def parse_log(log_file: str, out: str):
                             ).astype(float)
                         else:
                             step_name = curr_line.split(": ")[1].split(" C")[0]
-                            max_mem=np.max(mem, axis=0)
+                            max_mem = np.max(mem, axis=0)
                             output.write(f"\t{step_name}:\n\t\tRSS: {int(max_mem[0])}, VSZ: {int(max_mem[1])}, %MEM: {max_mem[2]}\n\n")
                             mem = np.array([[-1.0, -1.0, -1.0]])
                         curr_line = f.readline()
@@ -44,4 +45,8 @@ def parse_log(log_file: str, out: str):
 
 
 if __name__ == "__main__":
-    parse_log("dummy_log_multi_loops.log","memory_usage_summary.txt")
+    if len(sys.argv)!=3:
+        print("Usage: parse_log.py <log_file> <output file>")
+    else:
+        parse_log(sys.argv[1],sys.argv[2])
+        print(f"output file at {sys.argv[2]}")
