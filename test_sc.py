3fb0e3174e42988bb8b43df39817dd54e24ce4a0
nothing128 <mingyau.lam@gmail.com>
2025-05-14 15:17:24 -0400

    param tet

--- test_sc.py
+++ test_sc.py
@@ -28,7 +28,6 @@ num_threads_options = [-1]
 subset_options = [True, False]
 drop_X_options = [False]
 size_options = ['20K', '400K', '1M']
-size_options = ['20K']
 all_timers = []
 delay = 0.1
 params = itertools.product(
