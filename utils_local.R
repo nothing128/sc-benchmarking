8421e94270be3d9f3355bbf5214fb7cc73421990
nothing128 <mingyau.lam@gmail.com>
2025-05-21 16:01:50 -0400

    test

--- utils_local.R
+++ utils_local.R
@@ -41,7 +41,7 @@ TimerCollection = function(silent = TRUE) {
         aborted = aborted
       )
       print("-----------------------------------------")
-      run("kill", args = c(as.character(process_pid)))
+      processx::run("kill", args = c(as.character(process_pid)))
       stdout_output <- curr_process$read_all_output()
       print(stdout_output)
       
