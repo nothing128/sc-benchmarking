f1a4a6ec25ff2a7b009e7456e9b9536616fcb906
nothing128 <mingyau.lam@gmail.com>
2025-05-27 16:07:25 -0400

    update main script to take in command line args as options, small script to run multiple instances simultaneously

---
+++ run_sc_all.py
@@ -0,0 +1,12 @@
+from utils import run_slurm
+import itertools
+
+num_threads_options = [-1, 1]
+subset_options = ["True", "False"]
+size_options = ['20K', '400K', '1M']
+params = itertools.product(
+    size_options, num_threads_options, subset_options
+)
+
+for size, num_threads, subset in params:
+    run_slurm(f"python test_sc.py {num_threads} {size} {subset}")
--- test_sc.py
+++ test_sc.py
@@ -10,7 +10,7 @@ import time
 from utils_local import TimerMemoryCollection, system_info
 import matplotlib.pyplot as plt
 
-work_dir = 'projects/rrg-wainberg/lamming6/sc-benchmarking'
+work_dir = 'projects/sc-benchmarking'
 data_dir = 'single-cell/SEAAD/subsampled'
 log_file = 'memory_monitor.log'
 
@@ -26,128 +26,135 @@ if os.path.exists(log_file):
 system_info()
 
 num_threads_options = [-1, 1]
-subset_options = [True, False]
+subset_options = ["True", "False"]
 size_options = ['20K', '400K', '1M']
 all_timers = []
 delay = 0.15
-params = itertools.product(
-    size_options, num_threads_options, subset_options
-)
+num_threads = int(sys.argv[1])
+subset = (True if sys.argv[2]=="True" else False)
+size = sys.argv[3]
 
+if ("-h" in sys.argv) or \
+    (num_threads not in num_threads_options) or \
+    (sys.argv[2] not in subset_options) or\
+    (size not in size_options):
+    print(f"Usage: python {sys.argv[0]} [num_threads_options] [subset_options] [size_options]")
+    print("num_threads_options must be -1 or 1")
+    print("subset_options must be True or False")     
+    print("size_options must be 20K or 400K or 1M")
+    exit(1)
 
+timers = TimerMemoryCollection(silent=True)
 
-for size, num_threads, subset in params:
-    timers = TimerMemoryCollection(silent=True)
+# Note: Loading is much slower from $scratch disk
 
-    # Note: Loading is much slower from $scratch disk
+with timers('Load data'):
+    data = SingleCell(
+        f'{data_dir}/SEAAD_raw_{size}.h5ad',
+        num_threads=1)
+    data = data.set_num_threads(num_threads)
+print(f'X num_threads: {data.X._num_threads}')
+
+# Note: QC filters are matched across libraries for timing, then
+# standardized by filtering to single_cell.py qc cells
+with timers('Quality control'):
+    data.qc(
+        subset=subset,
+        max_mito_fraction=0.05,
+        min_genes=100,
+        nonzero_MALAT1=False,
+        remove_doublets=False,
+        allow_float=True,
+        verbose=False,
+        num_threads=num_threads)
+# Not timed
+if subset:
+    data = data\
+        .filter_obs(pl.col('tmp_passed_QC'))\
+        .with_uns(QCed=True)
+else:
+    data = data\
+        .rename_obs({'tmp_passed_QC': 'passed_QC'})\
+        .with_uns(QCed=True)
+with timers('Doublet detection'):
+    data = data.find_doublets(
+        batch_column='sample',
+        num_threads=num_threads)
+print(f'cells: {data.shape[0]}, genes: {data.shape[1]}')
+
+with timers('Feature selection'):
+    data = data.hvg(
+        num_threads=num_threads)
     
-    with timers('Load data'):
-        data = SingleCell(
-            f'{data_dir}/SEAAD_raw_{size}.h5ad',
-            num_threads=1)
-        data = data.set_num_threads(num_threads)
-    print(f'X num_threads: {data.X._num_threads}')
-    
-    # Note: QC filters are matched across libraries for timing, then
-    # standardized by filtering to single_cell.py qc cells
-    with timers('Quality control'):
-        data.qc(
-            subset=subset,
-            max_mito_fraction=0.05,
-            min_genes=100,
-            nonzero_MALAT1=False,
-            remove_doublets=False,
-            allow_float=True,
-            verbose=False,
-            num_threads=num_threads)
-    # Not timed
-    if subset:
-        data = data\
-            .filter_obs(pl.col('tmp_passed_QC'))\
-            .with_uns(QCed=True)
-    else:
-        data = data\
-            .rename_obs({'tmp_passed_QC': 'passed_QC'})\
-            .with_uns(QCed=True)
-    with timers('Doublet detection'):
-        data = data.find_doublets(
-            batch_column='sample',
-            num_threads=num_threads)
-    print(f'cells: {data.shape[0]}, genes: {data.shape[1]}')
-    
-    with timers('Feature selection'):
-        data = data.hvg(
-            num_threads=num_threads)
-        
-    with timers('Normalization'):
-        data = data.normalize(
-            num_threads=num_threads)
+with timers('Normalization'):
+    data = data.normalize(
+        num_threads=num_threads)
 
-    with timers('PCA'):
-        data = data.PCA(num_threads=num_threads)
+with timers('PCA'):
+    data = data.PCA(num_threads=num_threads)
 
-    # Not timed
-    if not subset:
-        data = data.filter_obs(pl.col('passed_QC'))
+# Not timed
+if not subset:
+    data = data.filter_obs(pl.col('passed_QC'))
 
-    with timers('KNN'):
-        data = data.neighbors(num_threads=num_threads)
+with timers('KNN'):
+    data = data.neighbors(num_threads=num_threads)
 
-    with timers('SNN'):
-        data = data.shared_neighbors(num_threads=num_threads)
+with timers('SNN'):
+    data = data.shared_neighbors(num_threads=num_threads)
 
-    # TODO: The number of clusters needs to match across libraries
-    with timers('Clustering (3 resolutions)'):
-        data = data.cluster(
-            resolution=[1, 0.5, 2],
-            num_threads=num_threads)
+# TODO: The number of clusters needs to match across libraries
+with timers('Clustering (3 resolutions)'):
+    data = data.cluster(
+        resolution=[1, 0.5, 2],
+        num_threads=num_threads)
 
-    print(f'cluster_0: {len(data.obs["cluster_0"].unique())}')
-    print(f'cluster_1: {len(data.obs["cluster_1"].unique())}')
-    print(f'cluster_2: {len(data.obs["cluster_2"].unique())}')
+print(f'cluster_0: {len(data.obs["cluster_0"].unique())}')
+print(f'cluster_1: {len(data.obs["cluster_1"].unique())}')
+print(f'cluster_2: {len(data.obs["cluster_2"].unique())}')
 
-    # TODO: Swap neighbor graphs with scanpy to assess
-    # embedding differences
-    with timers('Embedding'):
-        data = data.embed(
-            num_threads=num_threads)
+# TODO: Swap neighbor graphs with scanpy to assess
+# embedding differences
+with timers('Embedding'):
+    data = data.embed(
+        num_threads=num_threads)
 
-    with timers('Plot embeddings'):
-        data.plot_embedding(
-            'cluster_0',
-            f'{work_dir}/figures/sc_embedding_cluster_{size}.png')
+with timers('Plot embeddings'):
+    data.plot_embedding(
+        'cluster_0',
+        f'{work_dir}/figures/sc_embedding_cluster_{size}.png')
 
-    # Not timed
-    with timers('Find markers'):
-        markers = data.find_markers(
-            'cluster_0',
-            num_threads=num_threads)
+# Not timed
+with timers('Find markers'):
+    markers = data.find_markers(
+        'cluster_0',
+        num_threads=num_threads)
 
-    # with timers('Save data'):
-    #    data.save(
-    #        f'{data_dir}/test_write.h5ad',
-    #        overwrite=True)
+# with timers('Save data'):
+#    data.save(
+#        f'{data_dir}/test_write.h5ad',
+#        overwrite=True)
 
-    # Not timed
-    # os.remove(f'{data_dir}/test_write.h5ad')
+# Not timed
+# os.remove(f'{data_dir}/test_write.h5ad')
 
-    print('--- Params ---')
-    print(f'{size=}, {num_threads=}, {subset=}')
-    timers.print_summary(sort=False)
+print('--- Params ---')
+print(f'{size=}, {num_threads=}, {subset=}')
+timers.print_summary(sort=False)
 
-    df = timers\
-        .to_dataframe(sort=False, unit='s')\
-        .with_columns(
-            pl.lit('basic').alias('vignette'),
-            pl.lit(size).alias('size'),
-            pl.lit(num_threads).alias('num_threads'),
-            pl.lit(subset).alias('subset'))
+df = timers\
+    .to_dataframe(sort=False, unit='s')\
+    .with_columns(
+        pl.lit('basic').alias('vignette'),
+        pl.lit(size).alias('size'),
+        pl.lit(num_threads).alias('num_threads'),
+        pl.lit(subset).alias('subset'))
 
-    all_timers.append(df)
-    del data, timers, df; gc.collect()
+all_timers.append(df)
+del data, timers, df; gc.collect()
 
 timers_df = pl.concat(all_timers)
-timers_df.write_csv(f'{work_dir}/output/test_basic_sc_all.csv')
+timers_df.write_csv(f'{work_dir}/output/test_basic_sc_{size}_{("single_thread" if num_threads == 1  else "multi_thread")}{("_subset" if subset  else "_no_subset")}.csv')
 
 '''
 --- System Information ---
