4b4ff104ecfe3d928397d0ac127cd3aaa17190c8
nothing128 <mingyau.lam@gmail.com>
2025-05-28 13:55:52 -0400

    reorder help msg

--- test_sc.py
+++ test_sc.py
@@ -25,6 +25,23 @@ if os.path.exists(log_file):
 num_threads_options = [-1, 1]
 subset_options = ["True", "False"]
 size_options = ['20K', '400K', '1M']
+
+
+system_info()
+
+if len(sys.argv != 4):
+    print(f"Usage: python {sys.argv[0]} [num_threads_options] [subset_options] [size_options]")
+    print("num_threads_options must be -1 or 1")
+    print("subset_options must be True or False")     
+    print("size_options must be 20K or 400K or 1M")
+    exit(1)
+    
+all_timers = []
+delay = 0.15
+num_threads = int(sys.argv[1])
+subset = (True if sys.argv[2]=="True" else False)
+size = sys.argv[3]
+
 if ("-h" in sys.argv) or \
     (num_threads not in num_threads_options) or \
     (sys.argv[2] not in subset_options) or\
@@ -34,15 +51,6 @@ if ("-h" in sys.argv) or \
     print("subset_options must be True or False")     
     print("size_options must be 20K or 400K or 1M")
     exit(1)
-
-system_info()
-
-
-all_timers = []
-delay = 0.15
-num_threads = int(sys.argv[1])
-subset = (True if sys.argv[2]=="True" else False)
-size = sys.argv[3]
 
 
 
