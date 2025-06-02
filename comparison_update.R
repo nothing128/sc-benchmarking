e4ac4d642392ac40209ab82698e5cdb6e76d6a65
nothing128 <mingyau.lam@gmail.com>
2025-06-02 16:42:55 -0400

    also outputs memory usage plot now

--- comparison_update.R
+++ comparison_update.R
@@ -10,7 +10,7 @@ work_dir <- 'output'
 
 results <- bind_rows( # Use bind_rows() here
   lapply(
-    list.files(path = "output", pattern = "test_basic_", full.names = TRUE),
+    list.files(path = "output", pattern = "test_", full.names = TRUE),
     function(file_path) {
       # Read CSV, keeping original names and strings as characters
       df <- read.csv(file_path, check.names = FALSE, stringsAsFactors = FALSE)
@@ -87,7 +87,8 @@ my_colors <- c("test_basic_sc_multi_thread_subset" = "#ff0000",
                "test_basic_scanpy_single_thread_subset" = "#CCCC00", 
                "test_seurat_bpcells_single_thread_subset" = "#0000ff",
                "test_basic_sc_single_thread_no_subset" = '#006400',
-               "test_basic_sc_single_thread_subset" = '#00bb00'
+               "test_basic_sc_single_thread_subset" = '#00bb00',
+               "test_basic_seurat_single_thread_subset" = "#00008B"
                
                ) 
 
@@ -98,8 +99,19 @@ p_1 <- results %>%
   scale_x_break(c(50, 100), scales = 0.5) +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_discrete(limits = rev(levels(results$operation))) +
-  scale_fill_manual(values = my_colors) +
-  labs(x = "Duration (s)", y = "") +
+  scale_fill_manual(
+    name = "model type",
+    values = my_colors,
+    labels = c("test_basic_sc_multi_thread_subset" = "multithreaded with subset",   
+               "test_basic_sc_multi_thread_no_subset" = "multithreaded without subset",
+               "test_basic_scanpy_single_thread_subset" = "scanpy", 
+               "test_seurat_bpcells_single_thread_subset" = "seurat using bpcells",
+               "test_basic_sc_single_thread_no_subset" = 'single threaded without subset',
+               "test_basic_sc_single_thread_subset" = 'single threaded with subset',
+               "test_basic_seurat_single_thread_subset" = "seurat"
+               
+    ) # Item labels
+  ) +
   theme_classic() +
   theme(legend.position = "right") +
   ggtitle("20K")
@@ -108,8 +120,9 @@ p_2 <- results %>%
   filter(size == "400K") %>%
   ggplot(aes(x = duration, y = operation, fill = fill_group)) +
   geom_bar(stat = "identity", position = "dodge") +
+  scale_x_break(c(250, 400), scales = 0.5) +
   scale_x_break(c(1000, 2200), scales = 0.5) +
-  scale_x_break(c(3100, 4500), scales = 0.25) +
+  
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_discrete(limits = rev(levels(results$operation))) +
   scale_fill_manual(values = my_colors) +
@@ -122,7 +135,7 @@ p_3 <- results %>%
   filter(size == "1M") %>%
   ggplot(aes(x = duration, y = operation, fill = fill_group)) +
   geom_bar(stat = "identity", position = "dodge") +
-  scale_x_break(c(200, 900), scales = "free") +
+  scale_x_break(c(450, 900), scales = "free") +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_discrete(limits = rev(levels(results$operation))) +
   scale_fill_manual(values = my_colors, name = "fill_group") +
@@ -132,7 +145,85 @@ p_3 <- results %>%
   ggtitle("1M")
 
 combined_plot <- aplot::plot_list(p_1, p_2, p_3, ncol = 1)
-p_1
 ggsave("figures/comparison.png", plot = combined_plot, width = 10, height = 12)
+
+p <- ggplot(results, aes(x = memory, y = operation, fill = fill_group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_y_discrete(limits = rev(levels(results$operation))) +
+  scale_x_continuous(expand = c(0, 0)) +
+  # coord_cartesian(xlim = c(0, 50)) +
+  labs(x = "Duration (s)", y = "", fill = "Number of cells") +
+  theme_classic() +
+  theme(
+    axis.text.y = element_text(hjust = 0),
+    axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(~size, scales = "free_x", ncol = 1)
+
+ggsave("figures/memory_comparison.png", plot = p, width = 10, height = 8)
+
+
+
+my_colors <- c("test_basic_sc_multi_thread_subset" = "#ff0000",   
+               "test_basic_sc_multi_thread_no_subset" = '#8b0000',
+               "test_basic_scanpy_single_thread_subset" = "#CCCC00", 
+               "test_seurat_bpcells_single_thread_subset" = "#0000ff",
+               "test_basic_sc_single_thread_no_subset" = '#006400',
+               "test_basic_sc_single_thread_subset" = '#00bb00',
+               "test_basic_seurat_single_thread_subset" = "#00008B"
+               
+) 
+
+p_1 <- results %>% 
+  filter(size == "20K") %>%
+  ggplot(aes(x = memory, y = operation, fill = fill_group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_discrete(limits = rev(levels(results$operation))) +
+  labs(title="memory usage", x="memory usage (GiB)", y="") +
+  scale_fill_manual(
+    name = "model type",
+    values = my_colors,
+    labels = c("test_basic_sc_multi_thread_subset" = "multithreaded with subset",   
+               "test_basic_sc_multi_thread_no_subset" = "multithreaded without subset",
+               "test_basic_scanpy_single_thread_subset" = "scanpy", 
+               "test_seurat_bpcells_single_thread_subset" = "seurat using bpcells",
+               "test_basic_sc_single_thread_no_subset" = 'single threaded without subset',
+               "test_basic_sc_single_thread_subset" = 'single threaded with subset',
+               "test_basic_seurat_single_thread_subset" = "seurat"
+               
+    ) # Item labels
+  ) +
+  theme_classic() +
+  theme(legend.position = "right") +
+  ggtitle("20K")
+
+p_2 <- results %>% 
+  filter(size == "400K") %>%
+  ggplot(aes(x = memory, y = operation, fill = fill_group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+
+  
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_discrete(limits = rev(levels(results$operation))) +
+  scale_fill_manual(values = my_colors) +
+  labs(title="memory usage", x="memory usage (GiB)", y="") +
+  theme_classic() +
+  theme(legend.position = "none") +
+  ggtitle("400K")
+
+p_3 <- results %>% 
+  filter(size == "1M") %>%
+  ggplot(aes(x = memory, y = operation, fill = fill_group)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_discrete(limits = rev(levels(results$operation))) +
+  scale_fill_manual(values = my_colors, name = "fill_group") +
+  labs(title="memory usage", x="memory usage (GiB)", y="") +
+  theme_classic() +
+  theme(legend.position = "none") +
+  ggtitle("1M")
+
+combined_plot <- aplot::plot_list(p_1, p_2, p_3, ncol = 1)
+ggsave("figures/memory_comparison.png", plot = combined_plot, width = 10, height = 12)
 
 
