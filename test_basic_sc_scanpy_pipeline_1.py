c95628af9c9d87174178ec49e77ca67d1f7ec8d4
nothing128 <mingyau.lam@gmail.com>
2025-06-10 14:46:24 -0400

    testing

--- test_basic_sc_scanpy_pipeline_1.py
+++ test_basic_sc_scanpy_pipeline_1.py
@@ -87,17 +87,13 @@ data_for_pca = data.copy()
 
 with timers('PCA'):
     data_for_pca.PCA() 
-print("PCA done")
 pca_result_matrix = data_for_pca._X.toarray() 
 
 data._obsm['X_pca'] = pca_result_matrix
-print("scanpy convert")
 anndata = data.to_scanpy()
-print("convert done")
 with timers('Neighbor graph'):
     sc.pp.neighbors(anndata)
 
-print("neighbor graph done")
 
 with timers('Embedding'):
     sc.tl.umap(anndata)
